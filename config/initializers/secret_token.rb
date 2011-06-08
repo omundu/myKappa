65062945ebe7392ac0c62a354406d95157a86dfe
Mwaki Magotswi <magotswi@gmail.com>
2011-06-08 04:58:04 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KDR::Application.config.secret_token = 'fd99744660ebdf3d17739b4388982d11ac70f28fa04daf1db3ec746a2be3e83cd2f56f149eaa5eea7036dc50b060342acdedfe7e047cd81c7edf7d57397c9e8a'
